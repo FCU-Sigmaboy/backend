3bc646829d70d9d13f731186bb5b61be89adf9cd
JIN <d1397026@o365.fcu.edu.tw>
2025-11-09 23:37:07 +0800

    新增followed_at

--- database/functions/get_userProfile (RPC).sql
+++ database/functions/get_userProfile (RPC).sql
@@ -5,6 +5,7 @@
 -- 獲取公開使用者個人檔案 (RPC)
 -- *** 已更新，加入 carbon_saved_kg ***
 -- *** 已更新，加入 created_at ***
+-- *** 已更新，加入是否已追蹤的欄位 followed_at ***
 CREATE
 OR
 REPLACE FUNCTION public.get_public_user_profile(p_user_id UUID -- 要查詢的使用者 ID
@@ -19,21 +20,23 @@ BEGIN
                 'nickname', u.nickname,
                 'profile_picture_url', u.profile_picture_url,
                 'avg_rating', u.avg_rating,
-
-            -- *** 新增的欄位 (來自 users 表) ***
                 'created_at', u.created_at,
-            -- *** 新增欄位 ***
                 'carbon_saved_kg', p.carbon_saved_kg,
-
-            -- 從 'following' 表計算「追蹤中」 (我追蹤了誰)
                 'following_count', (SELECT COUNT(*)
                                     FROM public.following
-                                    WHERE follower_id = u.id),
-
-            -- 從 'following' 表計算「追蹤者」 (誰追蹤了我)
+                                    WHERE follower_id = u.id -- u.id = p_user_id
+                ),
                 'followers_count', (SELECT COUNT(*)
                                     FROM public.following
-                                    WHERE following_id = u.id)
+                                    WHERE following_id = u.id -- u.id = p_user_id
+                ),
+            -- *** 新增的欄位 ***
+            -- 檢查 "我" (v_current_uid) 是否追蹤了 "這個人" (p_user_id)
+                'followed_at', (SELECT f.created_at
+                                FROM public.following f
+                                WHERE f.follower_id = v_current_uid -- "我"
+                                  AND f.following_id = p_user_id -- "這個 profile 的人"
+                )
         )
  FROM public.users u
           -- *** 新增 JOIN ***
