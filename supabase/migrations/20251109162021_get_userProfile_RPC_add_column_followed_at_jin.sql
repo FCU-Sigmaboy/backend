093f1e8723be0fa97871baaba4f11a05177137a3
JIN <d1397026@o365.fcu.edu.tw>
2025-11-10 14:55:58 +0800

    補上v_current_uid

--- supabase/migrations/20251109162021_get_userProfile_RPC_add_column_followed_at_jin.sql
+++ supabase/migrations/20251109162021_get_userProfile_RPC_add_column_followed_at_jin.sql
@@ -12,6 +12,8 @@ REPLACE FUNCTION public.get_public_user_profile(p_user_id UUID -- 要查詢的�
 )
     RETURNS JSON
     AS $$
+    DECLARE
+    v_current_uid UUID := auth.uid(); -- *** 獲取 "當前登入者" ID ***
 BEGIN
     -- 使用 json_build_object 來建構您需要的 DTO
     RETURN
